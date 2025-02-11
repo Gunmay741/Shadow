19315844b4edbf029aeff50797e2eac617c8e6c2
Gunmay741 <gkhan6@ocdsb.ca>
2025-02-11 09:42:01 -0500

    Update shadow.js

--- shadow.js
+++ shadow.js
@@ -12,7 +12,8 @@ startScreen.style.fontFamily = 'Helvetica, sans-serif';
 startScreen.style.color = 'white';
 startScreen.innerHTML = `
     <h1>Shadow's Adventure</h1>
-    <h1>All enemies have an invisible aura around them that will end your game!!</h1>
+    <h3>All enemies have an invisible aura</h3>
+    <h3>  around them that will end your game!! </h3>
     <h3> You can only press 1 key at a time </h3>
     <p>Press 'Enter' to Start</p>
     <p>Press 'E' for Easy Mode, 'H' for Hard Mode</p>
